84642a7603d63065804a9bf6d1c2f18f7f5d0485
scottCheezem <scott.cheezem@gmail.com>
2015-09-11 17:29:54 -0400

    updates to alamofire...aparently

--- DataCoordinator/APIClient.swift
+++ DataCoordinator/APIClient.swift
@@ -28,9 +28,10 @@ public class APIClient: NSObject {
     public var baseUrl : String = ""
     //should AnyClass be SyncableModel
     public func getDataForClass(aClass : APIClass, params:[String: AnyObject]? = nil, completionHandler:((success: Bool, results:[NSDictionary], error:Error?) -> Void)?) {
-        Alamofire.request(.GET, self.baseUrl+aClass.apiEndPointForClass, parameters:params, encoding:.JSON).responseJSON() {
-            (request, response, data) in
-            
+        Alamofire.request(.GET, self.baseUrl+aClass.apiEndPointForClass, parameters:params).responseJSON { (request, response, result) -> Void in
+            print("the request:", request)
+            print(response)
+            print(result)
         }
 
     }
